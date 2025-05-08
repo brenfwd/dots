4592eaed6428246b17f36d79ec17893e3497a6cd
Brenden Forward <44183550+brenfwd@users.noreply.github.com>
2025-05-08 06:37:22 -0400

    nvim: add telescope

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,7 @@
+return {
+    {
+        -- Telescope.nvim: fuzzy finding for everything
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+}
