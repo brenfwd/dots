cccf53432abc1191fa58baf9b945ea88241e4abb
Brenden Forward <44183550+brenfwd@users.noreply.github.com>
2025-05-28 13:24:46 -0700

    [nvim] revamp neovim config

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -4,4 +4,26 @@ return {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    {
+        'nvim-tree/nvim-tree.lua', tag = 'v1.12',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+    },
+    {
+        'neovim/nvim-lspconfig', tag = 'v2.2.0',
+    },
+    {
+        'nanozuki/tabby.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+    },
+    {
+        "gennaro-tedesco/nvim-possession",
+        dependencies = {
+            "ibhagwan/fzf-lua",
+        },
+        config = true,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons' }
+    },
 }
